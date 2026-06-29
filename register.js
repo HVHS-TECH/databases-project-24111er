4d6cface52548fffcb60c4500bda36d595ff7e4b
24111er <24111er@hvhs.school.nz>
2026-06-29 20:56:04 +0000

    30/6/26

---
+++ register.js
@@ -0,0 +1,101 @@
+
+
+/***** function testing() {
+firebase.database().ref('/Game1/').update({userName3 : { 
+    highScore : 45
+}}
+
+)
+console.log("Updated Database")
+
+} *****/
+
+
+var userUid = sessionStorage.getItem("currentUserUid");
+var userAge;
+var userName;
+var formUserName;
+var formUserAge;
+
+const CURRENT_PAGE = window.location.pathname.split("/").pop();
+
+
+function checkValidityOfInput() {
+
+    if (CURRENT_PAGE === "register.html") {
+        formUserName  = document.getElementById("usernamefield");
+        formUserAge = document.getElementById("agefield");
+        userName = formUserName.value;
+        userAge = Number(formUserAge.value);
+    } else if (CURRENT_PAGE === "menu.html") {
+        formUserName = document.getElementById("newUsernameField");
+        formUserAge = document.getElementById("newAgeField");
+        userName = formUserName.value;
+        userAge = Number(formUserAge.value);
+    }
+
+    console.log(userName + " : " + userAge)
+    console.log(userUid + ": Testing") 
+
+    if ((userName === null || userName === "" || userAge === 0 || userAge === null) && CURRENT_PAGE !== "menu.html") {
+        window.alert("Please fill in all input boxes correctly before submitting");
+    } else if (formUserName.checkValidity() === false) {
+        window.alert("Please insert the correct input type into this field");
+    } else if (formUserAge.checkValidity() === false) {
+        window.alert("Please only insert numbers into this field");
+    } else if (userAge < 13) {
+        window.alert("You have to be at least 13 years old to use this site");
+    } else if (userAge > 120) {
+        window.alert("Please insert an accurate age into this field")
+    } else {
+        firebase.database().ref('/userInfo/' + userUid + '/').update({
+            name : userName,
+            age : userAge
+        });
+        if (CURRENT_PAGE === "menu.html") {
+            console.log("Obtaining Info")
+            obtainUserInfo();
+        }
+    }
+
+}
+
+async function goToMenu() {
+    var snapshot = await firebase.database().ref('/userInfo/' + userUid + '/name/').once('value');
+    var savedUserName = snapshot.val();
+    if (savedUserName === null || savedUserName === "") {
+        window.alert("Please submit details first")
+    } else {
+        window.location.href = "./menu.html";
+    }
+}
+
+
+
+
+
+//  async function setUserName() {
+//     var snapshotN = await firebase.database().ref('/userInfo/' + userUid + "/name").once('value');
+//     saveUserName(snapshotN);
+// }
+
+// function saveUserName(snapshotN) {
+//     userName = snapshotN.val();
+//     console.log(userName)
+// }
+
+
+/***** function testing() {
+firebase.database().ref('/Game1/').update({userName3 : { 
+    highScore : 45
+}}
+
+)
+console.log("Updated Database")
+
+} *****/
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,101 +1,225 @@
 
-
-/***** function testing() {
-firebase.database().ref('/Game1/').update({userName3 : { 
-    highScore : 45
-}}
-
-)
-console.log("Updated Database")
-
-} *****/
+var displayName = sessionStorage.getItem("currentUserName");
+var usersInfo = {};
+var currentGame;
+var i;
 
 
-var userUid = sessionStorage.getItem("currentUserUid");
-var userAge;
-var userName;
-var formUserName;
-var formUserAge;
 
-const CURRENT_PAGE = window.location.pathname.split("/").pop();
+const WELCOME_MESSAGE = document.getElementById("welcomeMessage");
+const DISPLAYING_USER_NAME = document.getElementById("userNameDisplayed");
+const DISPLAY_PROFILE_PICTURE = document.getElementById("profilePictureDisplayed");
+const DISPLAY_LEADERBOARD_G1 = document.getElementById("displayLeaderBoardG1");
+const DISPLAY_LEADERBOARD_G2 = document.getElementById("displayLeaderBoardG2");
+const CHANGE_DETAILS_BUTTON = document.getElementById("changeUserDetailsButton");
+const CHANGE_DETAILS_POPUP = document.getElementById("changeUserDetailsPopup");
+const CHANGE_DETAILS_CLOSE = document.getElementById("changeUserDetailsClose");
+const SAVE_DETAILS_POPUP = document.getElementById("saveDetailsPopup");
+const DISPLAY_TOP_THREE_G1 = document.getElementById("displayTopThreeG1"); 
+const DISPLAY_TOP_THREE_G2 = document.getElementById("displayTopThreeG2");
 
 
-function checkValidityOfInput() {
+
+
+if (CURRENT_PAGE === "menu.html" || CURRENT_PAGE === "geodash1.html" || CURRENT_PAGE === "geodash2.html") {
+    obtainUserInfo();
+    // reAuthToGainInfo();
+}
+
+if (CURRENT_PAGE === "menu.html") {
+    readTopThreeScores();
+    window.addEventListener('load', () => {
+        const LOADING_SCREEN = document.getElementById("loadingOverlay");
+        const LOG_OUT_BUTTON = document.getElementById("logOutButtonInvisible")
+        setTimeout(() => {
+            LOADING_SCREEN.classList.add("fadeOutOfOverlay");
+            LOG_OUT_BUTTON.classList.add("logOutButtonVisible");
+        }, 3000);
+
+ 
+
+    } );
+
+}
+
+if (CURRENT_PAGE === "geodash1.html" || CURRENT_PAGE === "geodash2.html") {
+    readLeaderBoard();
+}
+
+async function reAuthToGainInfo() {
+    await logIn();
+    console.log(GLOBAL_user + "Testing")
+    // obtainUserInfo();
+
+}
+
+async function readTopThreeScores() {
+    for (i=1; i<3; i++) {
+        await firebase.database().ref("/Game"+ i + "/").orderByValue().limitToFirst(3).once('value', sortTopThree, errorHandler);
+        console.log("Running for loop " + i)
+    }
+
+}
+
+function sortTopThree(snapshot) {
+    snapshot.forEach(displayTopThree);
+
+}
+
+function displayTopThree(child) {
+    var leaderBoardCount = DISPLAY_TOP_THREE_G1.querySelectorAll("li").length;
+    console.log("Amount of elements in div: " + leaderBoardCount)
+    if (leaderBoardCount >= 3) {
+        currentGame = DISPLAY_TOP_THREE_G2;
+    } else {
+        currentGame = DISPLAY_TOP_THREE_G1;
+    }
+    var value = String(child.val());
+    var positiveValue = value.split("-").pop();
+    console.log(child.key + " got " + positiveValue + " points")
+    currentGame.innerHTML += "<li>" + child.key + " : " + positiveValue + "</li>";
+
+
+}
+
+
+
+async function obtainUserInfo() {
+    console.log(currentUserAuthInfo[0] + ": Uid, Test")
+    var snapshot = await firebase.database().ref("/userInfo/" + userUid + "/").once('value','',errorHandler);
+    saveUserInfo(snapshot);
+}
+
+function saveUserInfo(snapshot) {
+    usersInfo = snapshot.val();
+    console.log(usersInfo)
+    displayProfileName();
+    if (CURRENT_PAGE === "menu.html") {
+        console.log("you are on the menu page")
+        displayWelcome();
+    }   
+    
+}
+
+
+
+function checkHighScore() {
+    console.log("checking high score")
+    if (CURRENT_PAGE === "geodash1.html") {
+        if (usersInfo.mostRecentScoreGD1 < usersInfo.highScoreGD1 || usersInfo.highScoreGD1 === undefined || usersInfo.highScoreGD1 === null) {
+            console.log("new highscore")
+            firebase.database().ref("/userInfo/" + userUid + "/").update({highScoreGD1 : usersInfo.mostRecentScoreGD1});
+            firebase.database().ref("/Game1/").update({
+                [usersInfo.name] : usersInfo.mostRecentScoreGD1
+            });
+            readLeaderBoard();
+        }
+    } else if (CURRENT_PAGE === "geodash2.html") {
+        if (usersInfo.mostRecentScoreGD2 < usersInfo.highScoreGD2 || usersInfo.highScoreGD2 === undefined || usersInfo.highScoreGD2 === null) {
+            console.log("new highscore")
+            firebase.database().ref("/userInfo/" + userUid + "/").update({highScoreGD2 : usersInfo.mostRecentScoreGD2});
+            firebase.database().ref("/Game2/").update({
+                [usersInfo.name] : usersInfo.mostRecentScoreGD2
+            }); 
+            readLeaderBoard();
+        }
+
+    }
+}
+
+function displayWelcome() {
+    WELCOME_MESSAGE.innerHTML = "<h1>Welcome " + usersInfo.googleName + "</h1>";
+    
+}
+function displayProfileName() {
+    DISPLAYING_USER_NAME.innerHTML = "<h2><i>" + usersInfo.name + "</i></h2>";
+    DISPLAY_PROFILE_PICTURE.innerHTML = "<img src = " + usersInfo.photoURL + " alt = profile picture>";
+}
+
+
+
+function changeUserDetails() {
+    CHANGE_DETAILS_POPUP.showModal();
+    console.log("Displaying Data")
+    const FORM_NEW_USERNAME = document.getElementById("newUsernameField");
+    const FORM_NEW_USER_AGE = document.getElementById("newAgeField");
+    FORM_NEW_USERNAME.value = usersInfo.name;
+    FORM_NEW_USER_AGE.value = usersInfo.age;
+    console.log(FORM_NEW_USERNAME + ":" + FORM_NEW_USER_AGE)
     
 
-    if (CURRENT_PAGE === "register.html") {
-        formUserName  = document.getElementById("usernamefield");
-        formUserAge = document.getElementById("agefield");
-        userName = formUserName.value;
-        userAge = Number(formUserAge.value);
-    } else if (CURRENT_PAGE === "menu.html") {
-        formUserName = document.getElementById("newUsernameField");
-        formUserAge = document.getElementById("newAgeField");
-        userName = formUserName.value;
-        userAge = Number(formUserAge.value);
-    }
+}
 
-    console.log(userName + " : " + userAge)
-    console.log(userUid) 
-
-    if ((userName === null || userName === "" || userAge === 0 || userAge === null) && CURRENT_PAGE !== "menu.html") {
-        window.alert("Please fill in all input boxes correctly before submitting");
-    } else if (formUserName.checkValidity() === false) {
-        window.alert("Please insert the correct input type into this field");
-    } else if (formUserAge.checkValidity() === false) {
-        window.alert("Please only insert numbers into this field");
-    } else if (userAge < 13) {
-        window.alert("You have to be at least 13 years old to use this site");
-    } else if (userAge > 120) {
-        window.alert("Please insert an accurate age into this field")
+function saveNewDetails() {
+    const FORM_NEW_USERNAME = document.getElementById("newUsernameField").value;
+    const FORM_NEW_USER_AGE = Number(document.getElementById("newAgeField").value);
+    newUserAge = FORM_NEW_USER_AGE;
+    newUserName = FORM_NEW_USERNAME;
+    console.log(newUserName + ":" + newUserAge)
+    if (newUserAge === 0 || newUserAge === null) {
+        newUserAge = usersInfo.age;
+    } else if (newUserName === '' || newUserName === null) {
+        newUserName = usersInfo.name
     } else {
-        firebase.database().ref('/userInfo/' + userUid + '/').update({
-            name : userName,
-            age : userAge
-        });
-        if (CURRENT_PAGE === "menu.html") {
-            obtainUserInfo;
-        }
+        checkValidityOfInput();
+    }
+    
+}
+
+async function saveNewDetailsAndClose() {
+    await saveNewDetails();
+    SAVE_DETAILS_POPUP.close();
+    CHANGE_DETAILS_POPUP.close();
+}
+
+function closePopupDontSave() {
+    SAVE_DETAILS_POPUP.close();
+    CHANGE_DETAILS_POPUP.close();
+
+}
+
+
+function closePopup() {
+    const FORM_NEW_USERNAME = document.getElementById("newUsernameField").value;
+    const FORM_NEW_USER_AGE = Number(document.getElementById("newAgeField").value);
+    if (FORM_NEW_USERNAME !== "" && FORM_NEW_USERNAME !== usersInfo.name || FORM_NEW_USER_AGE !== 0 && FORM_NEW_USER_AGE !== usersInfo.age) {
+        SAVE_DETAILS_POPUP.showModal();
+    } else {
+        CHANGE_DETAILS_POPUP.close();
     }
 
 }
 
-async function goToMenu() {
-    var snapshot = await firebase.database().ref('/userInfo/' + userUid + '/name/').once('value');
-    var savedUserName = snapshot.val();
-    if (savedUserName === null || savedUserName === "") {
-        window.alert("Please submit details first")
-    } else {
-        window.location.href = "./menu.html";
+function readLeaderBoard() {
+    if (CURRENT_PAGE === "geodash1.html") {
+        currentGame = "Game1";
+    } else if (CURRENT_PAGE === "geodash2.html") {
+        currentGame = "Game2"
     }
+    firebase.database().ref("/" + currentGame + "/").orderByValue().once('value', sortLeaderBoard, errorHandler);
+
 }
 
+function sortLeaderBoard(snapshot) {
+    if (CURRENT_PAGE === "geodash1.html") {
+        currentGame = DISPLAY_LEADERBOARD_G1;
+    } else if (CURRENT_PAGE === "geodash2.html") {
+        currentGame = DISPLAY_LEADERBOARD_G2;
+    }
+    currentGame.innerHTML = "";
+    snapshot.forEach(displayLeaderBoard)
 
+}
 
+function displayLeaderBoard(child) {
+    if (CURRENT_PAGE === "geodash1.html") {
+        currentGame = DISPLAY_LEADERBOARD_G1;
+    } else if (CURRENT_PAGE === "geodash2.html") {
+        currentGame = DISPLAY_LEADERBOARD_G2;
+    }
+    var value = String(child.val());
+    var positiveValue = value.split("-").pop();
+    console.log(child.key + " got " + positiveValue + " points")
+    currentGame.innerHTML += "<p>" + child.key + " : " + positiveValue + "</p>";
 
-
-//  async function setUserName() {
-//     var snapshotN = await firebase.database().ref('/userInfo/' + userUid + "/name").once('value');
-//     saveUserName(snapshotN);
-// }
-
-// function saveUserName(snapshotN) {
-//     userName = snapshotN.val();
-//     console.log(userName)
-// }
-
-
-/***** function testing() {
-firebase.database().ref('/Game1/').update({userName3 : { 
-    highScore : 45
-}}
-
-)
-console.log("Updated Database")
-
-} *****/
-
-
-
-
-
+}
